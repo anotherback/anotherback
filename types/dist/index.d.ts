46f548c3c5b0327254c8c8d80385f08e7e2ce4eb
mathcovax <campani.mathieu@gmail.com>
2023-03-09 09:45:41 +0000

    correct property ctx

--- types/dist/index.d.ts
+++ types/dist/index.d.ts
@@ -1,6 +1,5 @@
 import Anotherback from "./lib/anotherback";
 import Token from "./lib/token";
-import { AccessCtx as AC, CheckerCtx as CC, RequestCtx as RC, SenderCtx as SC } from "./lib/ctx";
 
 declare class StaticMethod{
 	static addProperty(name: string, property: any): void;
@@ -8,10 +7,10 @@ declare class StaticMethod{
 	static addSetter(name: string, fnc: (arg: any) => void): void;
 }
 
-declare class AccessCtx extends AC extends StaticMethod{}
-declare class CheckerCtx extends CC extends StaticMethod{}
-declare class RequestCtx extends RC extends StaticMethod{}
-declare class SenderCtx extends SC extends StaticMethod{}
+declare class AccessCtx extends StaticMethod{}
+declare class CheckerCtx extends StaticMethod{}
+declare class RequestCtx extends StaticMethod{}
+declare class SenderCtx extends StaticMethod{}
 
 export {
     Anotherback,
